d27c04010da6246674277f34d3c629d3735919eb
felipeevalerio <felipeevalerio@outlook.com>
2021-09-13 12:49:56 -0300

    fixed db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 const {Pool} = require('pg')
 require('dotenv').config({ path: require('find-config')('.env') })
-const connectionString = "postgres://ipplfomgmnuxbj:79a7ecbd18cced39815f08468a9666386aeba0f9a7605cb4f3dfd99aa298e39d@ec2-34-228-154-153.compute-1.amazonaws.com:5432/d9i7hml2ula1oc"
 
 module.exports = new Pool ({
-    // host:process.env.DB_HOST,
-    // user:process.env.DB_USER,
-    // password:process.env.DB_PASSWORD,
-    // port:process.env.DB_PORT,
-    // database:process.env.DB_DATABASE
-    connectionString
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+    database:process.env.DB_DATABASE,
+    ssl:{
+        rejectUnauthorized:false
+    }
 })  
